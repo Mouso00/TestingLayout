eab6b7b339c464a996408494f36c9776723cad78
Mous <mousahmed00@gmail.com>
2023-01-25 15:47:45 +0200

    First commit

---
+++ TestingLayout/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestingLayout;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TestingLayout/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingLayout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
